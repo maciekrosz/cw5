7661b63ad70bae2ab7b22b80f6ba3c590ef22c0e
MACIEK <s33498@pjwstk.edu.pl>
2025-04-15 10:48:45 +0200

    zadanie 5

---
+++ Film.java
@@ -0,0 +1,25 @@
+public class Film extends MediaBiblioteczne {
+    private String rezyser;
+    private int czasTrwania;
+
+
+    public Film(String tytul, int rokWydania, String rezyser, int czasTrwania) {
+        super(tytul, rokWydania);
+        this.rezyser = rezyser;
+        this.czasTrwania = czasTrwania;
+    }
+
+    @Override
+    public void wyswietlInformacje() {
+        super.wyswietlInformacje();
+        System.out.println("Reżyser: " + rezyser + ", Czas trwania: " + czasTrwania + " min");
+    }
+
+    public void sprawdzCzasTrwania() {
+        if (czasTrwania > 120) {
+            System.out.println("To długi film (" + czasTrwania + " minut).");
+        } else {
+            System.out.println("To standardowy film (" + czasTrwania + " minut).");
+        }
+    }
+}
---
+++ MediaBiblioteczne.java
@@ -0,0 +1,37 @@
+public class MediaBiblioteczne {
+    protected String tytul;
+    protected int rokWydania;
+    protected boolean dostepny;
+
+    public MediaBiblioteczne(String tytul, int rokWydania) {
+        this.tytul = tytul;
+        this.rokWydania = rokWydania;
+        this.dostepny = true; // nowe medium jest dostępne
+    }
+
+
+    public void wypozycz() {
+        if (dostepny) {
+            dostepny = false;
+            System.out.println("Medium \"" + tytul + "\" zostało wypożyczone.");
+        } else {
+            System.out.println("Medium \"" + tytul + "\" jest już wypożyczone.");
+        }
+    }
+
+
+    public void zwroc() {
+        if (!dostepny) {
+            dostepny = true;
+            System.out.println("Medium \"" + tytul + "\" zostało zwrócone.");
+        } else {
+            System.out.println("Medium \"" + tytul + "\" nie było wypożyczone.");
+        }
+    }
+
+
+    public void wyswietlInformacje() {
+        String status = dostepny ? "Dostępne" : "Wypożyczone";
+        System.out.println("Tytuł: " + tytul + ", Rok wydania: " + rokWydania + ", Status: " + status);
+    }
+}
